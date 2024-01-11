7734d116152f25bd32850b5a660972b5eebe9109
Pedro Muniz <pedrolehimuniz@gmail.com>
2024-01-11 17:00:55 -0300

    SplideSlider Structure

---
+++ src/components/common/SlideComponent/index.tsx
@@ -0,0 +1,36 @@
+import { CourseType } from "@/src/services/couseService";
+import {
+  Splide,
+  SplideSlide,
+} from "../../../../node_modules/@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+import SlideCard from "../SlideCard";
+
+interface props {
+  course: CourseType[];
+}
+
+const SlideComponent = ({ course }: props) => {
+  return (
+    <>
+      <div>
+        <Splide
+          options={{
+            type: "loop",
+            perPage: 4,
+            perMove: 1,
+            pagination: false,
+          }}
+        >
+          {course?.map((course) => (
+            <SplideSlide key={course.id}>
+              <SlideCard course={course} />
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
+    </>
+  );
+};
+
+export default SlideComponent;
